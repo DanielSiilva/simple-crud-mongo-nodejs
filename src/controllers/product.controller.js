ae4d4df31cd6a1abc368134cfa663f56a55ed68a
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-29 14:30:51 -0300

    feat: function getProducts controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,9 +1,20 @@
 const Product = require("../models/product.model");
 
-const getProduct = async (req, res) => {
+const getProducts = async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//Recebe id pelo req => qualquer paramentro pelo id
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByd(id);
+    res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
