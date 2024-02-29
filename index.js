44f9d3a8408485d187dad5b365161b6b6e185a7b
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-29 14:24:02 -0300

    feat: models

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const Product = require("./src/models/product.model.js");
+const productRoute = require("./src/routes/product.route.js");
 
 //console.log("Valor da variável de ambiente DB_USER:", process.env.MONGOBD_URL);
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node Api Server Updated");
+});
 
 mongoose
   .connect(process.env.MONGOBD_URL)
