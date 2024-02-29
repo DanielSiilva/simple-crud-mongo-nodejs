a00dfd5b478ac02a20948f16d11e44e7eff328c9
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-29 14:00:25 -0300

    database configuration - mongodb

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
 
-console.log("Valor da variável de ambiente DB_USER:", process.env.MONGOBD_URL);
+//console.log("Valor da variável de ambiente DB_USER:", process.env.MONGOBD_URL);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.MONGOBD_URL)
+  .then(() => {
+    console.log("Connected to mongodb");
+    app.listen(3002, () => {
+      console.log("Server is running on port 3002");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
