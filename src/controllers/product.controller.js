19d779d1ebd07d2188ba30829d75c609f86a4c0f
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-29 14:35:30 -0300

    feat: function createProduct  controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -9,7 +9,7 @@ const getProducts = async (req, res) => {
   }
 };
 
-//Recebe id pelo req => qualquer paramentro pelo id
+//Recebe id pelo req.params => qualquer paramentro pelo id
 const getProduct = async (req, res) => {
   try {
     const { id } = req.params;
@@ -17,5 +17,16 @@ const getProduct = async (req, res) => {
     res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+//Created
+//Recebe dados pelo req.body => qualquer paramentro pelo id
+const createProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
