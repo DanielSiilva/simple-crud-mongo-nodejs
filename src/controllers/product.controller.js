fd60b2771d592d47a80048ae9f0eae63b1eb9fd1
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-29 14:38:39 -0300

    feat: function updateProduct  controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -30,3 +30,15 @@ const createProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//Update
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdUpdate(id, req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
