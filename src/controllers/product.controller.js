77de9adefcf798c625c8fc5d316b6d90c6105392
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-29 14:26:55 -0300

    feat: function getProduct controller

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,10 @@
+const Product = require("../models/product.model");
+
+const getProduct = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
